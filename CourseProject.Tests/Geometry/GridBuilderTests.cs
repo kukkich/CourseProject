e32f3d7ca7ddd9bbad128f58e4163ef6d21ae3a1
vitia <vitia.iezhovqqqq@gmail.com>
2022-10-23 13:23:59 +0700

    moved ElementNodeIndexes tests into GridBuilderTests

--- CourseProject.Tests/Geometry/GridBuilderTests.cs
+++ CourseProject.Tests/Geometry/GridBuilderTests.cs
@@ -67,5 +67,97 @@ namespace CourseProject.Tests.Geometry
 
             Assert.That(expected.SequenceEqual(computedGrid), Is.True);
         }
+
+        [Test]
+        public void TestNodeIndexes_Split_1X_To_1Y()
+        {
+            AxisSplitParameter splitting = new(1, 1);
+            var grid = _gridBuilder.Build(splitting);
+
+            var expected = new List<NodeIndexes>
+            {
+                new(0, 1, 2, 3)
+            };
+
+            Assert.That(expected.SequenceEqual(grid.ElementNodeIndexes), Is.True);
+        }
+
+        [Test]
+        public void TestNodeIndexes_Split_2X_To_2Y()
+        {
+            AxisSplitParameter splitting = new(2, 2);
+            var grid = _gridBuilder.Build(splitting);
+
+            var expected = new List<NodeIndexes>
+            {
+                new(0, 1, 3, 4),
+                new(1, 2, 4, 5),
+                new(3, 4, 6, 7),
+                new(4, 5, 7, 8)
+            };
+
+            Assert.That(expected.SequenceEqual(grid.ElementNodeIndexes), Is.True);
+        }
+
+        [Test]
+        public void TestNodeIndexes_Split_3X_To_2Y()
+        {
+            AxisSplitParameter splitting = new(3, 2);
+            var grid = _gridBuilder.Build(splitting);
+
+            var expected = new List<NodeIndexes>
+            {
+                new(0, 1, 4, 5),
+                new(1, 2, 5, 6),
+                new(2, 3, 6, 7),
+                new(4, 5, 8, 9),
+                new(5, 6, 9, 10),
+                new(6, 7, 10, 11),
+            };
+
+            Assert.That(expected.SequenceEqual(grid.ElementNodeIndexes), Is.True);
+        }
+
+        [Test]
+        public void TestNodeIndexes_Split_2X_To_4Y()
+        {
+            AxisSplitParameter splitting = new(2, 4);
+            var grid = _gridBuilder.Build(splitting);
+
+            var expected = new List<NodeIndexes>
+            {
+                new(0, 1, 3, 4),
+                new(1, 2, 4, 5),
+                new(3, 4, 6, 7),
+                new(4, 5, 7, 8),
+                new(6, 7, 9, 10),
+                new(7, 8, 10, 11),
+                new(9, 10, 12, 13),
+                new(10, 11, 13, 14),
+            };
+
+            Assert.That(expected.SequenceEqual(grid.ElementNodeIndexes), Is.True);
+        }
+
+        [Test]
+        public void TestNodeIndexes_Split_4X_To_2Y()
+        {
+            AxisSplitParameter splitting = new(4, 2);
+            var grid = _gridBuilder.Build(splitting);
+
+            var expected = new List<NodeIndexes>
+            {
+                new(0, 1, 5, 6),
+                new(1, 2, 6, 7),
+                new(2, 3, 7, 8),
+                new(3, 4, 8, 9),
+                new(5, 6, 10, 11),
+                new(6, 7, 11, 12),
+                new(7, 8, 12, 13),
+                new(8, 9, 13, 14),
+            };
+
+            Assert.That(expected.SequenceEqual(grid.ElementNodeIndexes), Is.True);
+        }
     }
 }
