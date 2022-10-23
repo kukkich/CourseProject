cdf8c14394c6affe16b72dae137ad694aa77d299
vitia <vitia.iezhovqqqq@gmail.com>
2022-10-23 13:17:52 +0700

    moved ElementNodeIndexes enumerable into Grid builder

--- BoundaryProblem/Geometry/Grid.cs
+++ BoundaryProblem/Geometry/Grid.cs
@@ -4,42 +4,17 @@
     {
         public List<Point2D> Nodes { get; private set; }
         public List<Element> Elements { get; private set; }
+        public List<NodeIndexes> ElementNodeIndexes { get; private set; }
 
-        public IEnumerable<NodeIndexes> ElementNodeIndexes
+        public Grid(
+            IEnumerable<Point2D> nodes, 
+            IEnumerable<Element> elements, 
+            IEnumerable<NodeIndexes> elementNodeIndexes
+        )
         {
-            get
-            {
-                var nodesPerXAxis = _splitParameter.XSteps + 1;
-                for (var i = 0; i < _splitParameter.YSteps; i++)
-                {
-                    var leftBottomStartIndex = i * nodesPerXAxis;
-                    
-                    var leftTopStartIndex = leftBottomStartIndex + nodesPerXAxis;
-
-                    for (var j = 0; j < _splitParameter.XSteps; j++)
-                    {
-                        var leftBottomIndex = leftBottomStartIndex + j;
-                        var leftTopIndex = leftTopStartIndex + j;
-
-                        yield return new NodeIndexes(
-                            leftBottomIndex,
-                            leftBottomIndex + 1,
-                            leftTopIndex,
-                            leftTopIndex + 1
-                            );
-                    }
-                }
-            }
-        }
-
-        private readonly AxisSplitParameter _splitParameter;
-
-
-        public Grid(IEnumerable<Point2D> nodes, IEnumerable<Element> elements, AxisSplitParameter splitParameter)
-        {
-            _splitParameter = splitParameter;
-            Nodes = nodes?.ToList();
-            Elements = elements?.ToList();
+            ElementNodeIndexes = elementNodeIndexes.ToList();
+            Nodes = nodes.ToList();
+            Elements = elements.ToList();
         }
     }
 }
--- BoundaryProblem/Geometry/GridBuilder.cs
+++ BoundaryProblem/Geometry/GridBuilder.cs
@@ -2,9 +2,11 @@
 {
     public class GridBuilder
     {
+
         private readonly Rectangle _area;
         private AxisSplitParameter _splitParameter;
         private Point2D _stepSize;
+
         public GridBuilder(Rectangle area)
         {
             _area = area;
@@ -20,19 +22,10 @@
             
 
             return new Grid(
-                GenerateNodes(),
-                GenerateElements(),
-                _splitParameter
+                GenerateNodes,
+                GenerateElements,
+                ElementNodeIndexes
                 );
-        }
-
-        public IEnumerable<Point2D> GetNodes(AxisSplitParameter splitParameter)
-        {
-            _stepSize = new Point2D(
-                CalcStep(_area.LeftBottom.X, _area.RightBottom.X, _splitParameter.XSteps),
-                CalcStep(_area.LeftBottom.Y, _area.LeftTop.Y, _splitParameter.YSteps)
-            );
-            return GenerateNodes();
         }
 
         private static double CalcStep(double loweBound, double upperBound, int stepsCount)
@@ -40,35 +33,67 @@
             return (upperBound - loweBound) / stepsCount;
         }
 
-        private IEnumerable<Point2D> GenerateNodes()
+        private IEnumerable<Point2D> GenerateNodes
         {
-            for (var i = 0; i <= _splitParameter.YSteps; i++)
+            get
             {
-                for (var j = 0; j <= _splitParameter.XSteps; j++)
+                for (var i = 0; i <= _splitParameter.YSteps; i++)
                 {
-                    var x = _area.LeftBottom.X + _stepSize.X * j;
-                    var y = _area.LeftBottom.Y + _stepSize.Y * i;
+                    for (var j = 0; j <= _splitParameter.XSteps; j++)
+                    {
+                        var x = _area.LeftBottom.X + _stepSize.X * j;
+                        var y = _area.LeftBottom.Y + _stepSize.Y * i;
 
-                    yield return new Point2D(x, y);
+                        yield return new Point2D(x, y);
+                    }
                 }
             }
         }
 
-        private IEnumerable<Element> GenerateElements()
+        private IEnumerable<Element> GenerateElements
         {
-            for (var i = 0; i < _splitParameter.YSteps; i++)
+            get
             {
-                for (var j = 0; j < _splitParameter.XSteps; j++)
+                for (var i = 0; i < _splitParameter.YSteps; i++)
                 {
-                    var x = _area.LeftBottom.X + _stepSize.X * j;
-                    var y = _area.LeftBottom.Y + _stepSize.Y * i;
+                    for (var j = 0; j < _splitParameter.XSteps; j++)
+                    {
+                        var x = _area.LeftBottom.X + _stepSize.X * j;
+                        var y = _area.LeftBottom.Y + _stepSize.Y * i;
 
-                    yield return new Element(new Rectangle(
-                        new Point2D(x, y),
-                        new Point2D(x + _stepSize.X, y),
-                        new Point2D(x, y + _stepSize.Y),
-                        new Point2D(x + _stepSize.X, y + _stepSize.Y)
-                    ));
+                        yield return new Element(new Rectangle(
+                            new Point2D(x, y),
+                            new Point2D(x + _stepSize.X, y),
+                            new Point2D(x, y + _stepSize.Y),
+                            new Point2D(x + _stepSize.X, y + _stepSize.Y)
+                        ));
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<NodeIndexes> ElementNodeIndexes
+        {
+            get
+            {
+                var nodesPerXAxis = _splitParameter.XSteps + 1;
+                for (var i = 0; i < _splitParameter.YSteps; i++)
+                {
+                    var leftBottomStartIndex = i * nodesPerXAxis;
+                    var leftTopStartIndex = leftBottomStartIndex + nodesPerXAxis;
+
+                    for (var j = 0; j < _splitParameter.XSteps; j++)
+                    {
+                        var leftBottomIndex = leftBottomStartIndex + j;
+                        var leftTopIndex = leftTopStartIndex + j;
+
+                        yield return new NodeIndexes(
+                            leftBottomIndex,
+                            leftBottomIndex + 1,
+                            leftTopIndex,
+                            leftTopIndex + 1
+                        );
+                    }
                 }
             }
         }
